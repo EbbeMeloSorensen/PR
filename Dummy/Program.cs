56bb45d6021c8dc6d442776a2ca5befa880f5f78
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2025-09-21 12:19:51 +0200

    POC for consuming home made nuget package

---
+++ Dummy/Program.cs
@@ -0,0 +1,8 @@
+﻿using Craft.Utils;
+using Newtonsoft.Json;
+
+var dummy1 = new LineSegmentD(new PointD(7, 9), new PointD(13, 24));
+var dummy2 = JsonConvert.SerializeObject(dummy1);
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
