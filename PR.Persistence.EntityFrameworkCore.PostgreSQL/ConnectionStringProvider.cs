8bcea4c8678199577fb97d9dbb6360e773c22118
Ebbe Melo Sørensen <ebs@dmi.dk>
2025-09-22 14:14:32 +0200

    Refactored ConnectioStringProvider for working with Heroku

--- PR.Persistence.EntityFrameworkCore.PostgreSQL/ConnectionStringProvider.cs
+++ PR.Persistence.EntityFrameworkCore.PostgreSQL/ConnectionStringProvider.cs
@@ -9,7 +9,30 @@ namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
 
         static ConnectionStringProvider()
         {
-            InitializeFromSettingsFile();
+            var env = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+            if (env == "Production")
+            {
+                // Use connection string provided at runtime by Heroku.
+                var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+                // Parse connection URL to connection string for Npgsql
+                connUrl = connUrl.Replace("postgres://", string.Empty);
+                var pgUserPass = connUrl.Split("@")[0];
+                var pgHostPortDb = connUrl.Split("@")[1];
+                var pgHostPort = pgHostPortDb.Split("/")[0];
+                var pgDb = pgHostPortDb.Split("/")[1];
+                var pgUser = pgUserPass.Split(":")[0];
+                var pgPass = pgUserPass.Split(":")[1];
+                var pgHost = pgHostPort.Split(":")[0];
+                var pgPort = pgHostPort.Split(":")[1];
+
+                _connectionString = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
+            }
+            else
+            {
+                InitializeFromSettingsFile();
+            }
         }
 
         public static void InitializeFromSettingsFile()
