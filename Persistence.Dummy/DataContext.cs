47b832886e11a42d3a78fb036b21bf0a28d6dd09
Ebbe Melo Sørensen <ebs@dmi.dk>
2025-09-24 09:45:54 +0200

    Added dummy proj for testing poc on 2 dbcontexts

---
+++ Persistence.Dummy/DataContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+//using PR.Persistence.EntityFrameworkCore;
+//using PR.Domain.Entities.PR;
+using PR.Domain.Entities.Smurfs;
+//using PR.Domain.Entities.C2IEDM.ObjectItems;
+
+namespace Persistence.Dummy
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(
+            DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Smurf> Smurfs { get; set; }
+
+        protected override void OnModelCreating(
+            ModelBuilder builder)
+        {
+            // PRDbContextBase.Versioned = true;
+            // PRDbContextBase.Configure(builder);
+
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
---
+++ Persistence.Dummy/Seed.cs
@@ -0,0 +1,25 @@
+using PR.Persistence.EntityFrameworkCore;
+
+namespace Persistence.Dummy
+{
+    public class Seed
+    {
+        public static async Task SeedData(
+            DataContext context)
+        {
+            if (!context.Smurfs.Any())
+            {
+                Seeding.CreateDataForSeeding(
+                    true, 
+                    out var people, 
+                    out var personComments,
+                    out var smurfs);
+
+                // await context.People.AddRangeAsync(people);
+                // await context.PersonComments.AddRangeAsync(personComments);
+                await context.Smurfs.AddRangeAsync(smurfs);
+                await context.SaveChangesAsync();
+            }
+        }
+    }
+}
