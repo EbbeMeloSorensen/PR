6460283a49f488dcd2ce6b0c0b7500dbe1cff602
Ebbe Melo Sørensen <ebs@dmi.dk>
2025-09-30 13:36:28 +0200

    -

--- PR.Persistence.EntityFrameworkCore.Sqlite/ConnectionStringProvider.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace PR.Persistence.EntityFrameworkCore.Sqlite
-{
-    public static class ConnectionStringProvider
-    {
-        public static string ConnectionString { get; set; } = "Data source=people.db";
-    }
-}
