b19a615866940076c9b71f6fc561a58d0ee6694d
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-10-06 17:59:57 +0200

    Added dummy Wpf app for preparing to combine with consolidated app layer

---
+++ DummyWpfApp/BootStrapper.cs
@@ -0,0 +1,25 @@
+﻿using StructureMap;
+
+namespace DummyWpfApp
+{
+    public class BootStrapper
+    {
+        public MainWindowViewModel MainWindowViewModel
+        {
+            get
+            {
+                try
+                {
+                    var mainWindowViewModel = Container.For<MainWindowViewModelRegistry>().GetInstance<MainWindowViewModel>();
+
+                    return mainWindowViewModel;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Unknown error: {e.Message}");
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ DummyWpfApp/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using GalaSoft.MvvmLight;
+
+namespace DummyWpfApp;
+
+public class MainWindowViewModel : ViewModelBase
+{
+    private string _greeting;
+
+    public string Greeting
+    {
+        get => _greeting;
+        set
+        {
+            _greeting = value;
+            RaisePropertyChanged();
+        }
+    }
+
+    public MainWindowViewModel()
+    {
+        _greeting = "Hello from MainWindowViewModel";
+    }
+}
+
