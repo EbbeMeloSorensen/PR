d5911d78dd551f5134acb346f93082d14d6a675b
Ebbe Melo Sorensen <ebbe.melo.sorensen@gmail.com>
2025-10-11 14:04:26 +0200

    -

--- DummyWpfApp2/App.xaml.cs
+++ DummyWpfApp2/App.xaml.cs
@@ -1,6 +1,13 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Windows;
+using Microsoft.EntityFrameworkCore;
+using PR.Persistence;
+using PR.Persistence.EntityFrameworkCore;
+using PR.Web.Application.Core;
+using PR.Web.Application.Interfaces;
+using PR.Web.Application.Smurfs;
+using PR.Web.Infrastructure.Pagination;
 
 namespace DummyWpfApp2
 {
@@ -11,30 +18,51 @@ namespace DummyWpfApp2
     {
         private IHost _host;
 
-        public App()
+        protected override void OnStartup(StartupEventArgs e)
         {
+            base.OnStartup(e);
+
             _host = Host.CreateDefaultBuilder()
                 .ConfigureServices((context, services) =>
                 {
+                    var connectionString = "Data source=babuska27.db";
+                    services.AddAppDataPersistence<PRDbContextBase>(options =>
+                        //options.UseSqlite(connectionString));
+                        options.UseSqlite(connectionString));
+
+            services.AddAutoMapper(assemblies: typeof(MappingProfiles).Assembly);
+                    services.AddApplication();   // registers MediatR and handlers
+                    services.AddScoped<IUnitOfWorkFactory, UnitOfWorkFactory>();
+                    services.AddScoped<IPagingHandler<SmurfDto>, PagingHandler<SmurfDto>>();
+
+                    services.AddMediatR(cfg =>
+                        cfg.RegisterServicesFromAssemblyContaining<List.Query>());
+
                     // Register our ViewModel and View
                     services.AddSingleton<MainWindowViewModel>();
-                    services.AddSingleton<MainWindow>();
+                    services.AddTransient<MainWindow>();
                 })
                 .Build();
-        }
 
-        protected override void OnStartup(StartupEventArgs e)
-        {
+            using var scope = _host.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<PRDbContextBase>();
+            //await db.Database.MigrateAsync();
+            //await Seeding.SeedDatabase(db);
+            db.Database.MigrateAsync().GetAwaiter().GetResult();
+            Seeding.SeedDatabase(db).GetAwaiter().GetResult();
+
             var mainWindow = _host.Services.GetRequiredService<MainWindow>();
             mainWindow.Show();
-
-            base.OnStartup(e);
         }
 
         protected override async void OnExit(ExitEventArgs e)
         {
-            await _host.StopAsync();
-            _host.Dispose();
+            if (_host is not null)
+            {
+                await _host.StopAsync();
+            }
+
+            _host?.Dispose();
             base.OnExit(e);
         }
     }
--- DummyWpfApp2/MainWindowViewModel.cs
+++ DummyWpfApp2/MainWindowViewModel.cs
@@ -1,23 +1,47 @@
-﻿using GalaSoft.MvvmLight;
+﻿using System.Collections.ObjectModel;
+using MediatR;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
+using PR.Web.Application.Smurfs;
 
 namespace DummyWpfApp2;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    private string _greeting;
+    private string _buttonText;
+    private readonly IMediator _mediator;
 
-    public string Greeting
+    public RelayCommand LoadSmurfsCommand { get; }
+
+    public string ButtonText
     {
-        get => _greeting;
+        get => _buttonText;
         set
         {
-            _greeting = value;
+            _buttonText = value;
             RaisePropertyChanged();
         }
     }
 
-    public MainWindowViewModel()
+    public ObservableCollection<SmurfDto> Smurfs { get; } = new();
+
+    public MainWindowViewModel(
+        IMediator mediator)
     {
-        _greeting = "Hello from MainWindowViewModel";
+        _buttonText = "Populate list with smurfs";
+        _mediator = mediator;
+
+        LoadSmurfsCommand = new RelayCommand(async () => await LoadSmurfsAsync());
+    }
+
+    private async Task LoadSmurfsAsync()
+    {
+        var smurfs = await _mediator.Send(new List.Query());
+
+        Smurfs.Clear();
+        foreach (var smurfDto in smurfs.Value)
+        {
+            Smurfs.Add(smurfDto);
+        }
     }
 }
