3535a21442e46bc304ce3f1e9cbbe92553b4c644
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-27 15:18:49 +0100

    -

---
+++ PR.Persistence.UnitTest/PersonRepositoryFacadeTest.cs
@@ -0,0 +1,221 @@
+﻿using FluentAssertions;
+using PR.Domain.Entities;
+using StructureMap;
+using WIGOS.Persistence.UnitTest;
+using Xunit;
+
+namespace PR.Persistence.UnitTest
+{
+    public class PersonRepositoryFacadeTest
+    {
+        private readonly UnitOfWorkFactoryFacade _unitOfWorkFactory;
+
+        public PersonRepositoryFacadeTest()
+        {
+            var container = Container.For<InstanceScanner>();
+
+            var unitOfWorkFactory = container.GetInstance<IUnitOfWorkFactory>();
+            unitOfWorkFactory.Reseed();
+
+            _unitOfWorkFactory = new UnitOfWorkFactoryFacade(unitOfWorkFactory);
+            _unitOfWorkFactory.DatabaseTime = null;
+        }
+
+        [Fact]
+        public async Task CreatePerson()
+        {
+            // Arrange
+            var person = new Person
+            {
+                FirstName = "Bamse"
+            };
+
+            // Act
+            using var unitOfWork1 = _unitOfWorkFactory.GenerateUnitOfWork();
+            await unitOfWork1.People.Add(person);
+            unitOfWork1.Complete();
+
+            // Assert
+            using var unitOfWork2 = _unitOfWorkFactory.GenerateUnitOfWork();
+            var people = await unitOfWork2.People.GetAll();
+            people.Count().Should().Be(3);
+
+            var expected = new List<string>
+            {
+                "Bamse",
+                "Kylo",
+                "Leia"
+            };
+
+            people.Select(_ => _.FirstName).OrderBy(_ => _).SequenceEqual(expected).Should().BeTrue();
+        }
+
+        [Fact]
+        public async Task GetLatestVersionOfPerson()
+        {
+            // Arrange
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var person = await unitOfWork.People.Get(new Guid("11223344-5566-7788-99AA-BBCCDDEEFF03"));
+
+            // Assert
+            person.FirstName.Should().Be("Leia");
+            person.Surname.Should().Be("Organa");
+            person.Nickname.Should().Be(null);
+        }
+
+        [Fact]
+        public async Task GetLatestVersionOfPersonIncludingAssociations()
+        {
+            // Arrange
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var person = await unitOfWork.People.GetIncludingPersonAssociations(
+                new Guid("11223344-5566-7788-99AA-BBCCDDEEFF03"));
+
+            // Assert
+            person.FirstName.Should().Be("Leia");
+            person.Surname.Should().Be("Organa");
+
+            person.ObjectPeople.Count().Should().Be(1);
+            person.ObjectPeople.Single().Description.Should().Be("is a parent of");
+            person.SubjectPeople.Count().Should().Be(0);
+        }
+
+        [Fact]
+        public async Task GetEarlierVersionOfPersonIncludingAssociations()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2015, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var person = await unitOfWork.People.GetIncludingPersonAssociations(
+                new Guid("11223344-5566-7788-99AA-BBCCDDEEFF03"));
+
+            // Assert
+            person.FirstName.Should().Be("Leia");
+            person.Surname.Should().Be("Organa");
+
+            person.ObjectPeople.Count().Should().Be(1);
+            person.ObjectPeople.Single().Description.Should().Be("is a parent of");
+            person.SubjectPeople.Count().Should().Be(1);
+            person.ObjectPeople.Single().Description.Should().Be("is a parent of");
+        }
+
+        [Fact]
+        public async Task GetLatestVersionOfPerson_AfterPersonWasDeleted_Throws()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2007, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var act = () => unitOfWork.People.Get(
+                new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00"));
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(act);
+            exception.Message.Should().Be("Tried retrieving person that did not exist at the given time");
+        }
+
+        [Fact]
+        public async Task GetEarlierVersionOfPerson_1()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2015, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var person = await unitOfWork.People.Get(
+                new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00"));
+
+            // Assert
+            person.FirstName.Should().Be("Darth");
+            person.Surname.Should().Be("Vader");
+        }
+
+        [Fact]
+        public async Task GetEarlierVersionOfPerson_2()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2013, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var person = await unitOfWork.People.Get(
+                new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00"));
+
+            // Assert
+            person.FirstName.Should().Be("Anakin");
+            person.Surname.Should().Be("Skywalker");
+        }
+
+        [Fact]
+        public async Task GetEarlierVersionOfPerson_BeforePersonWasCreated_Throws()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2007, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var act = () => unitOfWork.People.Get(
+                new Guid("11223344-5566-7788-99AA-BBCCDDEEFF00"));
+
+            // Assert
+            var exception = await Assert.ThrowsAsync<InvalidOperationException>(act);
+            exception.Message.Should().Be("Tried retrieving person that did not exist at the given time");
+        }
+
+        [Fact]
+        public async Task GetLatestVersionOfEntirePeopleCollection()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = null;
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var people = await unitOfWork.People.GetAll();
+
+            // Assert
+            people.Count().Should().Be(2);
+            people.Count(p => p.FirstName == "Leia").Should().Be(1);
+            people.Count(p => p.FirstName == "Kylo").Should().Be(1);
+
+        }
+
+        [Fact]
+        public async Task GetEarlierVersionOfEntirePersonCollection_1()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2017, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var people = await unitOfWork.People.GetAll();
+
+            // Assert
+            people.Count().Should().Be(4);
+            people.Count(p => p.FirstName == "Han").Should().Be(1);
+            people.Count(p => p.FirstName == "Luke").Should().Be(1);
+            people.Count(p => p.FirstName == "Leia").Should().Be(1);
+            people.Count(p => p.FirstName == "Ben").Should().Be(1);
+        }
+
+        [Fact]
+        public async Task GetEarlierVersionOfEntirePersonCollection_2()
+        {
+            // Arrange
+            _unitOfWorkFactory.DatabaseTime = new DateTime(2010, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+            using var unitOfWork = _unitOfWorkFactory.GenerateUnitOfWork();
+
+            // Act
+            var people = await unitOfWork.People.GetAll();
+
+            // Assert
+            people.Count().Should().Be(0);
+        }
+    }
+}
