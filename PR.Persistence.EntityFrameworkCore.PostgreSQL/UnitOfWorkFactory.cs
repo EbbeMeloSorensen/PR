c1e137dc59ad160949f7544c7a7f5313e6755940
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-27 15:05:30 +0100

    -

---
+++ PR.Persistence.EntityFrameworkCore.PostgreSQL/UnitOfWorkFactory.cs
@@ -0,0 +1,29 @@
+﻿namespace PR.Persistence.EntityFrameworkCore.PostgreSQL
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        public UnitOfWorkFactory()
+        {
+            using var context = new PRDbContext();
+            context.Database.EnsureCreated();
+            Seeding.SeedDatabase(context);
+        }
+
+        public IUnitOfWork GenerateUnitOfWork()
+        {
+            return new UnitOfWork(new PRDbContext());
+        }
+
+        public void Reseed()
+        {
+            using var context = new PRDbContext();
+            context.Database.EnsureCreated();
+
+            using var unitOfWork = GenerateUnitOfWork();
+            unitOfWork.PersonAssociations.Clear();
+            unitOfWork.People.Clear();
+            Seeding.SeedDatabase(context);
+            unitOfWork.Complete();
+        }
+    }
+}
