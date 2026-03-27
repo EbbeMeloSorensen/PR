0fdd2fd6f0cb5b1add3e1f480ecbd50c6189f262
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-27 15:09:00 +0100

    -

---
+++ PR.Persistence.EntityFrameworkCore.SqlServer/Migrations/20260327140845_InitialMigration.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PR.Persistence.EntityFrameworkCore.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Nickname = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ZipCode = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Dead = table.Column<bool>(type: "bit", nullable: true),
+                    ObjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Superseded = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonAssociations",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubjectPersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SubjectPersonObjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ObjectPersonId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ObjectPersonObjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ObjectId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Created = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Superseded = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonAssociations", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PersonAssociations_People_ObjectPersonId",
+                        column: x => x.ObjectPersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_PersonAssociations_People_SubjectPersonId",
+                        column: x => x.SubjectPersonId,
+                        principalTable: "People",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonAssociations_ObjectPersonId",
+                table: "PersonAssociations",
+                column: "ObjectPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonAssociations_SubjectPersonId",
+                table: "PersonAssociations",
+                column: "SubjectPersonId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PersonAssociations");
+
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
