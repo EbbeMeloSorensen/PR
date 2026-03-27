a9472b2a34689c3fddaf9f9eb93778338f7482d9
EbbeMeloSorensen <ebbe.melo.sorensen@gmail.com>
2026-03-27 15:22:25 +0100

    -

---
+++ PR.Persistenec.EntityFrameworkCore.InMemory/PRDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PR.Persistence.EntityFrameworkCore;
+
+namespace PR.Persistenec.EntityFrameworkCore.InMemory
+{
+    public class PRDbContext : PRDbContextBase
+    {
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseInMemoryDatabase("Dummy");
+        }
+    }
+}
---
+++ PR.Persistenec.EntityFrameworkCore.InMemory/UnitOfWorkFactory.cs
@@ -0,0 +1,32 @@
+﻿using PR.Persistence;
+using PR.Persistence.EntityFrameworkCore;
+
+namespace PR.Persistenec.EntityFrameworkCore.InMemory
+{
+    public class UnitOfWorkFactory : IUnitOfWorkFactory
+    {
+        public UnitOfWorkFactory()
+        {
+            using var context = new PRDbContext();
+            context.Database.EnsureCreated();
+            Seeding.SeedDatabase(context);
+        }
+
+        public IUnitOfWork GenerateUnitOfWork()
+        {
+            return new UnitOfWork(new PRDbContext());
+        }
+
+        public void Reseed()
+        {
+            using var context = new PRDbContext();
+            context.Database.EnsureCreated();
+
+            using var unitOfWork = GenerateUnitOfWork();
+            unitOfWork.PersonAssociations.Clear();
+            unitOfWork.People.Clear();
+            Seeding.SeedDatabase(context);
+            unitOfWork.Complete();
+        }
+    }
+}
